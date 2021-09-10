437a5b80674c30fdebf1d30a05aaf42b3e9dc430
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-09-10 15:58:13 +0530

    2.4.0

--- sunutils/sunutils.py
+++ sunutils/sunutils.py
@@ -5,7 +5,7 @@ from pyrogram.utils import MAX_CHANNEL_ID
 from pytgcalls import PyTgCalls
 import config
 import asyncio
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 group_call = PyTgCalls(client)
